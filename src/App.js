6afbfe445991a7c3d4fd8432e7f87bb16d7cef0d
Kaan Karahan <143435367+KaanKarahann@users.noreply.github.com>
2025-03-14 11:42:32 +0300

    test failed i guess

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import React from "react";
-import Homepage from "./pages/Homepage";
+import Homepage from "./pages/Homepage/Homepage";
 // src/App.js
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./Login/LoginPage";
+import LoginPage from "./pages/Login/LoginPage";
 
 function App() {
   return (
@@ -13,8 +12,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
       </Routes>
-    </Router>
-    <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
       </Routes>
--- src/index.js
+++ src/index.js
@@ -4,12 +4,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css"; // TailwindCSS'i dahil et
-import App from "./App";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
